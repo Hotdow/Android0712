9e31ade10c9234c10eaadf1dc15cea1198a8f1c0
ldg8910 <202020611@sonline20.sen.go.kr>
2021-07-12 11:51:11 +0900

    android ver

--- app/src/main/java/com/example/project30611/MainActivity.java
+++ app/src/main/java/com/example/project30611/MainActivity.java
@@ -10,15 +10,16 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView text1, text2;
-    CheckBox chkAgree;
+    Switch chkAgree;
     RadioGroup rGroup1;
-    RadioButton rdoDog, rdoCat, rdoRabbit;
-    Button btnOK;
+    RadioButton oreo, pie, q10;
+    Button exit,reset;
     ImageView imgPet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,47 +31,59 @@ public class MainActivity extends AppCompatActivity {
         chkAgree = findViewById(R.id.ChkAgree);
         text2 = findViewById(R.id.Text2);
         rGroup1 = findViewById(R.id.Rgroup1);
-        rdoDog = findViewById(R.id.RdoDog);
-        rdoCat = findViewById(R.id.RdoCat);
-        rdoRabbit = findViewById(R.id.RdoRabbit);
-        btnOK = findViewById(R.id.BtnOK);
+        oreo = findViewById(R.id.oreo);
+        pie = findViewById(R.id.pie);
+        q10 = findViewById(R.id.q10);
+        exit = findViewById(R.id.exit);
+        reset = findViewById(R.id.reset);
         imgPet = findViewById(R.id.ImgPet);
-
+        oreo.setOnClickListener(this);
+        pie.setOnClickListener(this);
+        q10.setOnClickListener(this);
+        exit.setOnClickListener(this);
+        reset.setOnClickListener(this);
         chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (chkAgree.isChecked()==true){
                     text2.setVisibility(View.VISIBLE);
                     rGroup1.setVisibility(View.VISIBLE);
-                    btnOK.setVisibility(View.VISIBLE);
+                    exit.setVisibility(View.VISIBLE);
+                    reset.setVisibility(View.VISIBLE);
                     imgPet.setVisibility(View.VISIBLE);
                 }
                 else{
                     text2.setVisibility(View.INVISIBLE);
                     rGroup1.setVisibility(View.INVISIBLE);
-                    btnOK.setVisibility(View.INVISIBLE);
+                    exit.setVisibility(View.INVISIBLE);
+                    reset.setVisibility(View.INVISIBLE);
                     imgPet.setVisibility(View.INVISIBLE);
                 }
             }
 
         });
-        btnOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (rGroup1.getCheckedRadioButtonId()){
-                    case R.id.RdoDog:
-                        imgPet.setImageResource(R.drawable.dog);
-                        break;
-                    case R.id.RdoCat:
-                        imgPet.setImageResource(R.drawable.cat);
-                        break;
-                    case R.id.RdoRabbit:
-                        imgPet.setImageResource(R.drawable.rabbit);
-                        break;
-                    default:
-                        Toast.makeText(MainActivity.this,"동물 먼저 선택하세요",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.oreo:
+                imgPet.setImageResource(R.drawable.oreo);
+                break;
+            case R.id.pie:
+                imgPet.setImageResource(R.drawable.pie);
+                break;
+            case R.id.q10:
+                imgPet.setImageResource(R.drawable.q10);
+                break;
+            case R.id.exit:
+                finish();
+                break;
+            case R.id.reset:
+                chkAgree.setChecked(false);
+                rGroup1.clearCheck();
+                imgPet.setImageResource(0);
+                break;
+        }
     }
 }
